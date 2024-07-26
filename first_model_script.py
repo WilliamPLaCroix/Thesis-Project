df48f39f6057f78e5b0e3747860d4fb52a1cdcaf
WilliamPLaCroix <williamplacroix@gmail.com>
2024-07-26 15:54:54 +0200

    change gpu name

--- first_model_script.py
+++ first_model_script.py
@@ -19,7 +19,7 @@ data_location = './data/wikilarge/'
 import os
 
 def get_freer_gpu():
-    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    os.system('nvidia-smi -q -d Memory |grep -Tesla GPU|grep Free >tmp')
     memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
     return np.argmax(memory_available)
 
