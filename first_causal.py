3ab524eea1d9618136c31f5af4bbeb9185e10bc5
WilliamPLaCroix <williamplacroix@gmail.com>
2024-08-19 15:41:13 +0200

    DSGF

--- first_causal.py
+++ first_causal.py
@@ -332,7 +332,7 @@ def main():
 
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding="max_length", max_length=128, label_pad_token_id=tokenizer.pad_token_id)
 
-    train_data_loader = torch.utils.data.DataLoader(tokenized_dataset['train'], batch_size=training_args.batch_size, shuffle=True, collate_fn=data_collator)
+    train_data_loader = torch.utils.data.DataLoader(tokenized_dataset['train'], batch_size=32, shuffle=True, collate_fn=data_collator)
     # eval_data_loader = torch.utils.data.DataLoader(tokenized_dataset['test'], batch_size=training_args.batch_size, shuffle=False, collate_fn=data_collator)
     # dataloaders = {'train': train_data_loader, 'eval': eval_data_loader}
 
