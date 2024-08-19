b038e2de13c340473e57ca9aa23b8d34c95c82db
WilliamPLaCroix <williamplacroix@gmail.com>
2024-08-19 17:21:02 +0200

    max_new_tokens

--- first_causal.py
+++ first_causal.py
@@ -34,7 +34,7 @@ data_location = './data/wikilarge/'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model_name = "openai-community/gpt2"
 config = AutoConfig.from_pretrained(model_name)
-config.update({"max_length": 1024})
+config.update({"max_new_tokens": 1024})
 model = AutoModelForCausalLM.from_pretrained(model_name, config=config)
 #model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
