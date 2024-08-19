aeba67462ef40681ec0782ec67559aded0a7eabc
WilliamPLaCroix <williamplacroix@gmail.com>
2024-08-19 13:52:51 +0200

    remove save_safetensors

--- first_causal.py
+++ first_causal.py
@@ -348,6 +348,7 @@ def main():
 
     training_args = TrainingArguments(
         output_dir="./models",
+        save_safetensors=False, # this is a temporary fix for a bug in the transformers library
         eval_strategy="epoch",
         learning_rate=2e-5,
         weight_decay=0.01,
