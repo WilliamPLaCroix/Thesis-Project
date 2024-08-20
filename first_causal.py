cd7eebc98fdfa3a62842b6e95bf7102257e9b012
WilliamPLaCroix <williamplacroix@gmail.com>
2024-08-20 13:10:42 +0200

    run grades 1-12

--- first_causal.py
+++ first_causal.py
@@ -34,15 +34,6 @@ data_location = './data/wikilarge/'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model_name = "openai-community/gpt2"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
-tokenizer.pad_token = tokenizer.eos_token
-
-config = AutoConfig.from_pretrained(model_name, pad_token_id=tokenizer.pad_token_id)
-model = AutoModelForCausalLM.from_pretrained(model_name, config=config)
-
-generation_config = GenerationConfig(max_length=256)
-generation_config.save_pretrained("./generation_config")
-
 
 # class FineTuneGPT2(nn.Module):
 #     def __init__(self, model, tokenizer, training_args):
@@ -56,38 +47,12 @@ generation_config.save_pretrained("./generation_config")
         
 #         return self.model(input_ids, attention_mask=attention_mask, labels=labels)
 
-"""
-Below function tokenizes parallel corpus into source:target pairs for Seq2Seq training
-"""
-# def tokenize_function(examples):
-#     return tokenizer(text=examples["source"], text_target=examples['target'], padding=True, truncation=True, max_length=1024, return_tensors="pt")
 
-"""
-Below function tokenizes parallel corpus into target only inputs for unsupervised fine-tuning
-"""
-def tokenize_function(examples):
-    return tokenizer(text=examples["target"], text_target=examples["target"], padding=True, truncation=True, max_length=1024, return_tensors="pt")
-
-
-def compute_metrics(prediction):
-    metric_name = "sari"
-    metric = load(metric_name)
-
-    labels_ids = prediction.label_ids
-    pred_ids = prediction.predictions
-    input_ids = prediction.inputs
-
-    # all unnecessary tokens are removed
-    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
-    references = [[reference] for reference in label_str]
-    source_str = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-    predictions_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-
-    score = metric.compute(sources=source_str, predictions=predictions_str, references=references)
-    return score
 
 
 def main():
+
+
 
     train_texts = pd.read_pickle(f'{data_location}train_texts.pkl')
     print("train texts read in")
@@ -103,61 +68,101 @@ def main():
     
 
     ### change dataset[N] where N is the grade group you want to train on
-    tokenized_dataset = datasets[12].map(tokenize_function, batched=True, batch_size=32,
-                                      remove_columns=['source', 'target', '__index_level_0__'])
-    print(tokenized_dataset)
+    for N in range(1, 12):
+        tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+        tokenizer.pad_token = tokenizer.eos_token
+
+        """
+        Below function tokenizes parallel corpus into source:target pairs for Seq2Seq training
+        """
+        # def tokenize_function(examples):
+        #     return tokenizer(text=examples["source"], text_target=examples['target'], padding=True, truncation=True, max_length=1024, return_tensors="pt")
+
+        """
+        Below function tokenizes parallel corpus into target only inputs for unsupervised fine-tuning
+        """
+        def tokenize_function(examples):
+            return tokenizer(text=examples["target"], text_target=examples["target"], padding=True, truncation=True, max_length=1024, return_tensors="pt")
 
 
-    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding="max_length", max_length=128, label_pad_token_id=tokenizer.pad_token_id)
+        def compute_metrics(prediction):
+            metric_name = "sari"
+            metric = load(metric_name)
 
-    #train_data_loader = torch.utils.data.DataLoader(tokenized_dataset['train'], batch_size=32, shuffle=True, collate_fn=data_collator)
-    # eval_data_loader = torch.utils.data.DataLoader(tokenized_dataset['test'], batch_size=training_args.batch_size, shuffle=False, collate_fn=data_collator)
-    # dataloaders = {'train': train_data_loader, 'eval': eval_data_loader}
+            labels_ids = prediction.label_ids
+            pred_ids = prediction.predictions
+            input_ids = prediction.inputs
 
-    # for batch in train_data_loader:
-    #     print(batch.keys())
-    #     print(batch['input_ids'].shape)
-    #     print(batch['attention_mask'].shape)
-    #     print(batch['labels'].shape)
-    #     print(batch['target_grade'].shape)
-    #     break
-    print("data collated")
+            # all unnecessary tokens are removed
+            label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+            references = [[reference] for reference in label_str]
+            source_str = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+            predictions_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+
+            score = metric.compute(sources=source_str, predictions=predictions_str, references=references)
+            return score
+
+        config = AutoConfig.from_pretrained(model_name, pad_token_id=tokenizer.pad_token_id)
+        model = AutoModelForCausalLM.from_pretrained(model_name, config=config)
+
+        generation_config = GenerationConfig(max_length=256)
+        generation_config.save_pretrained("./generation_config")
+
+        tokenized_dataset = datasets[N].map(tokenize_function, batched=True, batch_size=32,
+                                        remove_columns=['source', 'target', '__index_level_0__'])
+        print(tokenized_dataset)
 
 
-    training_args = Seq2SeqTrainingArguments(
-        save_strategy="epoch", # turn off saving while testing
-        output_dir="./models",
-        overwrite_output_dir=True,
-        save_safetensors=False, # this is a temporary fix for a bug in the transformers library
-        #save_only_model=True,
-        save_total_limit=1,
-        eval_strategy="epoch",
-        learning_rate=2e-5,
-        weight_decay=0.01,
-        seed=42,
-        num_train_epochs=10,
-        load_best_model_at_end=True,
-        #prediction_loss_only=True,
-        metric_for_best_model="loss",
-        #greater_is_better=False,
-        label_names=["labels"],
-        include_inputs_for_metrics=True,
-        predict_with_generate=True,
-        generation_config=generation_config,
-    )
-    
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_dataset['train'],
-        eval_dataset=tokenized_dataset['test'],
-        data_collator=data_collator,
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
-        compute_metrics=compute_metrics,
-    )
+        data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding="max_length", max_length=128, label_pad_token_id=tokenizer.pad_token_id)
 
-    trainer.train()
-    trainer.save_model("./models/gpt_new-grade_12")
+        #train_data_loader = torch.utils.data.DataLoader(tokenized_dataset['train'], batch_size=32, shuffle=True, collate_fn=data_collator)
+        # eval_data_loader = torch.utils.data.DataLoader(tokenized_dataset['test'], batch_size=training_args.batch_size, shuffle=False, collate_fn=data_collator)
+        # dataloaders = {'train': train_data_loader, 'eval': eval_data_loader}
+
+        # for batch in train_data_loader:
+        #     print(batch.keys())
+        #     print(batch['input_ids'].shape)
+        #     print(batch['attention_mask'].shape)
+        #     print(batch['labels'].shape)
+        #     print(batch['target_grade'].shape)
+        #     break
+        print("data collated")
+
+
+        training_args = Seq2SeqTrainingArguments(
+            save_strategy="epoch",
+            output_dir="./models",
+            overwrite_output_dir=True,
+            save_safetensors=False, # this is a temporary fix for a bug in the transformers library
+            #save_only_model=True,
+            save_total_limit=1,
+            eval_strategy="epoch",
+            learning_rate=2e-5,
+            weight_decay=0.01,
+            seed=42,
+            num_train_epochs=10,
+            load_best_model_at_end=True,
+            #prediction_loss_only=True,
+            metric_for_best_model="loss",
+            #greater_is_better=False,
+            label_names=["labels"],
+            include_inputs_for_metrics=True,
+            predict_with_generate=True,
+            generation_config=generation_config,
+        )
+        
+        trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=tokenized_dataset['train'],
+            eval_dataset=tokenized_dataset['test'],
+            data_collator=data_collator,
+            callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+            compute_metrics=compute_metrics,
+        )
+
+        trainer.train()
+        trainer.save_model(f"./models/gpt_new-grade_{N}")
     return
 
 if __name__ == "__main__":
