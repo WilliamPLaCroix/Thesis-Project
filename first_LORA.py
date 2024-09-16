0bb41954d2a17d3a8a7e06e2b6906895477d88a1
WilliamPLaCroix <williamplacroix@gmail.com>
2024-09-16 17:10:13 +0200

    remove local save strategies

--- first_LORA.py
+++ first_LORA.py
@@ -156,22 +156,22 @@ def main():
 
 
         training_args = Seq2SeqTrainingArguments(
-            save_strategy="epoch",
-            output_dir="/models",
+            #save_strategy="epoch",
+            #output_dir="/models",
             report_to="wandb",  # enable logging to W&B
             run_name=f"gpt2-grade-{N}",  # name of the W&B run (optional)
             logging_steps=1,  # how often to log to W&B
             hub_model_id="williamplacroix/text-simplification",  # save the model to the Hub after training
-            overwrite_output_dir=True,
-            save_safetensors=False, # this is a temporary fix for a bug in the transformers library
+            #overwrite_output_dir=True,
+            #save_safetensors=False, # this is a temporary fix for a bug in the transformers library
             #save_only_model=True,
-            save_total_limit=1,
-            eval_strategy="epoch",
+            #save_total_limit=1,
+            #eval_strategy="epoch",
             learning_rate=2e-5,
             weight_decay=0.01,
             seed=42,
             num_train_epochs=2,
-            load_best_model_at_end=True,
+            #load_best_model_at_end=True,
             prediction_loss_only=True,
             metric_for_best_model="loss",
             #greater_is_better=False,
@@ -189,7 +189,7 @@ def main():
             train_dataset=tokenized_dataset['train'],
             eval_dataset=tokenized_dataset['test'],
             data_collator=data_collator,
-            callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+            #callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
             #compute_metrics=compute_metrics,
             tokenizer=tokenizer,
             
