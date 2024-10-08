f8134e62231beba65ceac81b58f7f3bbc57064bc
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-08 17:07:51 +0200

    better print statemetn

--- lora_merge_eval.py
+++ lora_merge_eval.py
@@ -45,6 +45,7 @@ def main(test_set_grade, model_a_proportion):
 
     current_model_name = f"g{test_set_grade-1}-{int(model_a_proportion*100)}_merge_g{test_set_grade+1}-{int(model_b_proportion*100)}_eval-on-g{test_set_grade}"
     print(f"Model name: {current_model_name}")
+    print(f"Model proportions: {model_a_proportion}:{model_b_proportion}")
     print("#"*50)
 
     os.environ["WANDB_PROJECT"] = "Graded text simplification evaluation"  # name your W&B project
