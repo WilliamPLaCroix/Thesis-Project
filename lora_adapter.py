c79c01553fbedb07ddaad513841f16c168f2cc6f
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-09 14:46:26 +0200

    push_)to_hub_mode_id

--- lora_adapter.py
+++ lora_adapter.py
@@ -78,13 +78,14 @@ def main(N):
     print("data collated")
 
     #current_model_name = f"gpt2-grade-{N}"
-    current_model_name = "model6-for-directory-testing"
+    current_model_name = "model7-for-directory-testing"
  
     training_args = TrainingArguments(
         logging_strategy="epoch",
         save_strategy="epoch",
         eval_strategy="epoch",
-        output_dir=f"./models/text-simplification/model6",
+        output_dir=f"williamplacroix/text-simplification",
+        push_to_hub_model_id=current_model_name,
         report_to="wandb",  # enable logging to W&B
         run_name=current_model_name,  # name of the W&B run (optional)
         logging_steps=1,  # how often to log to W&B
@@ -100,6 +101,7 @@ def main(N):
         num_train_epochs=1, ### CHANGE THIS BACK TO 5
         load_best_model_at_end=True,
         remove_unused_columns=False,
+        #push_to_hub=True,
     )
     
     trainer = Trainer(
@@ -112,7 +114,7 @@ def main(N):
     )
 
     trainer.train()
-    trainer.push_to_hub(current_model_name)
+    trainer.push_to_hub()
     return
 
 if __name__ == "__main__":
