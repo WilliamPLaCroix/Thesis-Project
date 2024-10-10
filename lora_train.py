594107079715a025fda1119f453f41c460e884fa
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-11 00:03:49 +0200

    no overwrite

--- lora_train.py
+++ lora_train.py
@@ -109,7 +109,7 @@ def main(model_grade):
         save_strategy="epoch",
         eval_strategy="epoch",
         output_dir=f"williamplacroix/text-simplification",
-        overwrite_output_dir=True,
+        #overwrite_output_dir=True,
         save_total_limit=1,
         report_to="wandb",  # enable logging to W&B
         run_name=current_model_name,  # name of the W&B run (optional)
