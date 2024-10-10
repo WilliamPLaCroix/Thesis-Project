9d0ffb966a86d964791294245c91bc3a4d5e5244
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-10 19:29:35 +0200

    model_id

--- lora_train.py
+++ lora_train.py
@@ -66,9 +66,9 @@ def main(model_grade):
                             )
     adapter_config = PeftConfig.from_pretrained("./")
     model = PeftModel.from_pretrained(model=model, 
-                                      #model_id="williamplacroix/text-simplification/gpt2-2-12-evens",
+                                      model_id="williamplacroix/text-simplification/gpt2-2-12-evens",
                                       config=adapter_config,
-                                      adapter_name="williamplacroix/text-simplification/gpt2-2-12-evens",
+                                      adapter_name="gpt2-2-12-evens",
                                       is_trainable=False,
                                       )
 
