020454ceb9de81f399a517c8401acef2745cce13
WilliamPLaCroix <williamplacroix@gmail.com>
2025-01-31 21:54:09 +0100

    comment out add to git credential

--- lora_baseline_adapter.py
+++ lora_baseline_adapter.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     load_dotenv()
     wandb.login(key=os.getenv("wandb"))
-    login(token=os.getenv("huggingface"), add_to_git_credential=True)
+    login(token=os.getenv("huggingface"))#, add_to_git_credential=True)
 
     #all_evens = sys.argv[1]
     all_evens = "all"
