a5f31bdf347b0c2f712d93de121993802531b77b
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-03 15:24:19 +0100

    add //

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /nethome/wlacroix/anaconda3/bin
+cd /nethome/wlacroix/anaconda3/bin/
 source activate thesis
-cd /nethome/wlacroix/Thesis-Project
+cd /nethome/wlacroix/Thesis-Project/
 HF_HOME=./.config python3 lora_baseline_adapter.py
