e262beb085ed2a4fd79ad6a47f6ea83874dc7064
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-03 16:08:06 +0100

    ideal setup

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cd /nethome/wlacroix/anaconda3/bin/
-source activate thesis
-cd /nethome/wlacroix/Thesis-Project/
+# cd /nethome/wlacroix/anaconda3/bin/
+# source activate thesis
+# cd /nethome/wlacroix/Thesis-Project/
+
+conda activate thesis
+
 HF_HOME=./.config python3 lora_baseline_adapter.py
