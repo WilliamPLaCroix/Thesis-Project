7f9501b2383b8722e011eafa2ad69f790f182bfe
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-03 15:22:52 +0100

    add conda env to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-conda activate thesis
-
-HF_HOME=./.config python3 Thesis-Project/lora_baseline_adapter.py
+cd /nethome/wlacroix/anaconda3/bin
+source activate thesis
+cd /nethome/wlacroix/Thesis-Project
+HF_HOME=./.config python3 lora_baseline_adapter.py
