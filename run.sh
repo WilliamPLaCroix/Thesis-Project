19a3c8c716f0a5862838776a258dbcb508ac6ff4
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-08 21:22:30 +0100

    with conda activate

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#. /nethome/wlacroix/miniconda3/bin/activate thesis
-# source activate thesis
 cd /nethome/wlacroix/Thesis-Project/
-# conda activate thesis
-# cd /nethome/wlacroix/Thesis-Project/
+conda activate thesis
 #HF_HOME=./.config python3 lora_baseline_adapter.py
 python3 helloworld.py
