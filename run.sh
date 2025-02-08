c5d3b4a58d8be4d81ad15b3b4c68fa7b78e145c3
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-08 21:21:05 +0100

    testing without conda activate

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@
 cd /nethome/wlacroix/Thesis-Project/
 # conda activate thesis
 # cd /nethome/wlacroix/Thesis-Project/
-HF_HOME=./.config python3 lora_baseline_adapter.py
+#HF_HOME=./.config python3 lora_baseline_adapter.py
+python3 helloworld.py
