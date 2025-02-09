c7a5eef54f91ab7758611646f49251f511b1a4f5
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-09 22:38:46 +0100

    remove 3 and 5

--- helper.py
+++ helper.py
@@ -61,7 +61,7 @@ def finetune_adapters() -> None:
     import lora_finetune
     odd_even = "odd"
     grades = {"even": {2, 4, 6, 8, 10, 12},
-                "odd": {3, 5, 7, 9, 11},
+                "odd": {7, 9, 11},
                 "all": {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
                 }
     model_grades: set = grades[odd_even]
