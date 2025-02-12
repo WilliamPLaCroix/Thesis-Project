f51781d08f226f055eb9632b8dd9729fdbfa089a
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-12 17:22:51 +0100

    back to single file

--- run-odd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. /nethome/wlacroix/miniconda3/bin/activate thesis
-cd /nethome/wlacroix/Thesis-Project/
-export WANDB_CACHE_DIR=/scratch/wlacroix/.cache/wandb/
-export HF_HOME=/scratch/wlacroix/.cache/huggingface/
-
-python3 helper.py --helper to
