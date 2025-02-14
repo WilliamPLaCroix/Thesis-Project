04a86a2dfa54804574831c03bdf8d4b7a190495a
WilliamPLaCroix <williamplacroix@gmail.com>
2025-02-14 13:20:55 +0100

    add gpu rename

---
+++ experiments_sarubi/rename_gpus.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# we need to rename gpus in order to access them via CUDA_VISIBLE_DEVICES
+
+new_devices=""
+IFS=',' read -ra my_array <<< "$CUDA_VISIBLE_DEVICES"
+for id in ${my_array[@]};
+do
+    new_devices=${new_devices}`nvidia-smi -L | grep $id | sed -E "s/^GPU ([0-9]+):.*$/\1/"`,
+done
+export CUDA_VISIBLE_DEVICES=${new_devices%?}
+echo "now: CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
